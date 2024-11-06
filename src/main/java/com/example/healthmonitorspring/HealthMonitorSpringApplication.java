fbd2ab789c7b7a58d4d10ece666c2ffc9b385193
BilalMO <azbilal.motassim@gmail.com>
2024-11-06 12:39:17 +0100

    init

---
+++ src/main/java/com/example/healthmonitorspring/HealthMonitorSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.healthmonitorspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthMonitorSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthMonitorSpringApplication.class, args);
+    }
+
+}
